5ccb9bac5ce7a52768cf45982402c2c9a639f951
deshawnbrown1111 <p51243140@gmail.com>
2025-11-25 03:16:19 -0500

    Add configuration file for game settings

---
+++ src/config.lua
@@ -0,0 +1,22 @@
+-- // Configuration File
+-- // Load this first: loadstring(game:HttpGet("CONFIG_URL"))()
+
+local Config = {}
+
+-- Account Settings
+Config.MAIN_ACCOUNT = "bloodyhound_lilljeff"
+Config.ALTS = {"fgsdgfgsdgsdgsdgs", "sgsgsghssrghs"} -- ONLY alts
+
+-- Server Settings
+Config.SERVER_URL = "http://localhost:5000"
+
+-- UI Settings
+Config.OPEN_KEY = Enum.KeyCode.K  -- Press K to open command bar
+
+-- Theme Settings
+Config.altSettings = {
+    ["Theme"] = "Dark"
+}
+
+return Config
+
